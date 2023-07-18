31435718b172b23eaebdad413554f9da55a75f5e
marissaposner <marissaposner14@gmail.com>
2023-07-18 11:39:04 +0200

    installing scarb

---
+++ Comet/target/release/Comet.d
@@ -0,0 +1,1 @@
+/Users/marissaposner/Comet/Comet/target/release/Comet: /Users/marissaposner/Comet/Comet/src/main.rs
---
+++ Comet/target/release/deps/Comet-e3e695ca9e02df98.d
@@ -0,0 +1,5 @@
+/Users/marissaposner/Comet/Comet/target/release/deps/Comet-e3e695ca9e02df98: src/main.rs
+
+/Users/marissaposner/Comet/Comet/target/release/deps/Comet-e3e695ca9e02df98.d: src/main.rs
+
+src/main.rs:
